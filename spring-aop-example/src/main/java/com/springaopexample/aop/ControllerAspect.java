021ec80cc5c05d60f306d24bc345473adf2077a3
junwvwv <wvwv0612@gmail.com>
2023-02-19 18:55:48 +0900

    controller aspect

--- spring-aop-example/src/main/java/com/springaopexample/aop/ControllerAspect.java
+++ spring-aop-example/src/main/java/com/springaopexample/aop/ControllerAspect.java
@@ -24,9 +24,22 @@ public class ControllerAspect {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
                 .getRequestAttributes()).getRequest();
 
+        /**
+         * JoinPoint
+         * - 어드바이스가 적용될 수 있는 위치
+         * - 스프링 AOP 는 프록시 방식을 사용하기 떄문에 메소드 실행 지점으로 제한된다
+         *
+         * JoinPoint & ProceedingJoinPoint 인터페이스
+         */
+        log.info("Signature = {}", joinPoint.getSignature()); //메소드 시그니처(이름, 매개변수 등) 리턴
+        log.info("Object = {}", joinPoint.getTarget()); //해당 클래스 객체 리턴
+        log.info("Object[] = {}", joinPoint.getArgs()); //메소드에 넘긴 인자 목록을 리턴
+
         try{
             stopWatch.start();
-            return joinPoint.proceed();
+            //Before
+            return joinPoint.proceed(); //메소드를 실행하고 리턴값을 받는다.
+            //after
         } catch (Throwable ex){
             throw ex;
         }finally{
